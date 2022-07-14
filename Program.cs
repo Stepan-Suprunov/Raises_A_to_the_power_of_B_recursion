a00eb6b3007c2fecc9ac94c0c03ebff7455b493a
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-14 23:45:50 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Возвести А в целую степень В с помощью рекурсии
+
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    
+    return a * Pow(a, b-1);
+}
+
+int A = 3;
+int B = 5;
+
+Console.Write(Pow(A, B));
